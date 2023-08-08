05e8d4cfc659436e50bdaa795668e3b27377f16b
eugene kimani <ekiukkim90@gmail.com>
2023-08-08 14:32:55 -0700

    python2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 10):
     for b in range(a+1, 10):
         if a == 8 and b == 9:
-            print(f"{a}{b}")
+            print("{}{}".format(a, b))
         else:
-            print(f"{a}{b}", end=",")
+            print("{}{}".format(a, b), end=",")
