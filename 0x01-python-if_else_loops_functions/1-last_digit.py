6136c5a77732f8d833501bb6d51b6bf7ba3c1665
eugene kimani <ekiukkim90@gmail.com>
2023-08-08 13:15:44 -0700

    python2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ last_y = "Last digit of"
 la_ty = abs(number) % 10
 
 if la_ty > 5:
-    print(f"{last_y} {number} is {la_ty} and is greater than 5")
+    print("{} {} is {} and is greater than 5".format(last_y, number, la_ty))
 elif la_ty == 0:
-    print(f"{last_y} {number} is {la_ty} and is 0")
+    print("{} {} is {} and is 0".format(last_y, number, la_ty))
 else:
-    print(f"{last_y} {number} is {la_ty} and is less than 6 and not 0")
+    print("{} {} is {} and is less than 6 and not 0".format(last_y, number, la_ty))
