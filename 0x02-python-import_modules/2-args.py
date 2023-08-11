914d72408f5bc1d5957a12772997b677bd12edd6
eugene kimani <ekiukkim90@gmail.com>
2023-08-11 14:05:32 -0700

    python3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    no_of_argvss = len(sys.argv) - 1
+
+    if no_of_argvss == 0:
+        print("0 arguments")
+    elif no_of_argvss == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(no_of_argvss))
+    for c in range(no_of_argvss):
+        print("{}: {}".format(no_of_argvss, c))
