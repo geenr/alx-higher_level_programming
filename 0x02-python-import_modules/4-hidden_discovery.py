f32d4616a121f1b706955db6ffd4accb0dbbab27
eugene kimani <ekiukkim90@gmail.com>
2023-08-11 15:10:47 -0700

    python3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     length = len(names)
-    for 1 in range(0, length):
-        if names[i].startwith("_"):
+    for i in range(0, length):
+        if names[i].startswith("_"):
             continue
         else:
             print("{}".format(names[i]))
