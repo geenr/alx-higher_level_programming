bc8c41c480b0b24ee8a810d70bf9498b41c3f0d9
eugene kimani <ekiukkim90@gmail.com>
2023-08-29 12:12:54 -0700

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size = 0):
+        self.size = size
+    def size(self):
+        return (self.__size)
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        return (self.__size * self.__size)
