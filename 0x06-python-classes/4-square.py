9e9cd5dbaf0cb4b2057d59b315bde4138756fa34
eugene kimani <ekiukkim90@gmail.com>
2023-08-29 15:04:12 -0700

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
+    """Realize a square"""
+
     def __init__(self, size = 0):
+        """Initialize a new square.
+
+        Args:
+            size (int): Size of new square.
+        """
+
         self.size = size
     @property
     def size(self):
+        """Sets current size of the square."""
         return (self.__size)
     @size.setter
     def size(self, value):
@@ -15,4 +25,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
+        """Returns current area of square."""
         return (self.__size * self.__size)
