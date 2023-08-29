12ef2e7bad2018af3df9861f615c35180c6b6aa8
eugene kimani <ekiukkim90@gmail.com>
2023-08-29 13:24:26 -0700

    classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,8 +3,10 @@
 class Square:
     def __init__(self, size):
         self.size = size
+    @property
     def size(self):
         return (self.__size)
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -14,8 +16,8 @@ class Square:
     def area(self):
         return (self.__size * self.__size)
     def my_print(self):
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
+        for a in range(0, self.__size):
+            [print("#", end="") for b in range(self.__size)]
             print()
         if self.__size == 0:
             print()
