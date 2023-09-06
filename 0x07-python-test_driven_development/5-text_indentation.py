e5315d7e3da66d5322b4ed8aee6261eeafadab36
eugene kimani <ekiukkim90@gmail.com>
2023-09-06 13:38:17 -0700

    tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defining a function that prints 2 new lines after a character '.' '?' and ':'."""
+def text_indentation(text):
+    """
+    Prints 2 new line after character '.' '?' and ':'.
+
+    Args:
+        text (str): The string with the characters.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    a = 0
+    while a < len(text) and text[a] == '':
+        a += 1
+
+    while a < len(text):
+        print(text[a], end="")
+        if text[a] == "\n" or text[a] in ".?:":
+            if text[a] in ".?:":
+                print("\n")
+            a += 1
+            while a < len(text) and text[a] == '':
+                a += 1
+            continue
+        a += 1
