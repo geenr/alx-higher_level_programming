49c7f9cf1f875e0db87d4c80d2df24b7dc84e7dc
eugene kimani <ekiukkim90@gmail.com>
2023-09-06 13:10:26 -0700

    tests

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,7 +14,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be >= 0.")
+        raise ValueError("size must be >= 0")
     for a in range(size):
         [print("#", end="") for i in range(size)]
         print("")
