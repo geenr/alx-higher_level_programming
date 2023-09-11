e90853a60023c902803fcf8570d509b60a41eb03
eugene kimani <ekiukkim90@gmail.com>
2023-09-11 14:52:46 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Declaring a function that returns the list of available attributes and methods of an object."""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: Object.
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,7 @@
+this is the project about inheritance
+eg class parent(object):
+	{body}
+   class child(parent):
+	{body}
+inheritance helps form a hireachial way of using other classes without tampering with the original class
+dir() is used to list all methods and objects used in a class
