7a67b3bd255076dfdf8c22b1c41ad1f9b1075d8e
eugene kimani <ekiukkim90@gmail.com>
2023-09-11 16:02:22 -0700

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Declaring a function that returns true if object is inherited."""
+def inherits_from(obj, a_class):
+    """
+    Return true if object is inherited.
+
+    Args:
+        obj: Inherited object being tested.
+        a_class: Class being tested.
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
