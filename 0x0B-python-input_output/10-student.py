20485765808449911f72523bf5a7cd8b5761a87c
eugene kimani <ekiukkim90@gmail.com>
2023-09-12 14:26:38 -0700

    readfile

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student class.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        Args:
+            attrs (list): The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(j) == str for j in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
