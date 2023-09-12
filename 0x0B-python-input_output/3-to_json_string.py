d36571bb4c013d10fd5623794b38f356a2d0d63f
eugene kimani <ekiukkim90@gmail.com>
2023-09-12 11:57:37 -0700

    readfile

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a function to return json."""
+
+import json
+def to_json_string(my_obj):
+    """
+    Returns an object represented by a json string.
+
+    Args:
+        my_str: The string being tested.
+    """
+    return (json.dumps(my_obj))
