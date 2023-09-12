dc47c463a4b1159dc220413cf14c039f374fe695
eugene kimani <ekiukkim90@gmail.com>
2023-09-12 13:47:50 -0700

    readfile

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Script that adds all arguments to a Python list."""
 import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
