1dd55cc2f029264d172256536fda95807ca7a800
eugene <ekiukkim90@gmail.com>
2024-04-10 10:10:37 -0700

    java

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let lenVar = process.argv.length;
+
+if (lenVar === 2)
+{
+	console.log('No argument');
+}
+else if (lenVar === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Argument found');
+}
